c0f7bd7b277063d14c37512e8c2af2f88a1bbfb3
YonghoAhn <hrh1110@naver.com>
2017-03-14 20:41:36 +0900

    All Key Hook, Blocked.

--- lock_client/lock_client/Form1.Designer.cs
+++ lock_client/lock_client/Form1.Designer.cs
@@ -29,32 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(921, 363);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Disable";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(921, 402);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Enable\r\n";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // timer1
             // 
@@ -66,11 +45,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Noto Sans CJK KR DemiLight", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(298, 142);
+            this.label1.Size = new System.Drawing.Size(284, 108);
             this.label1.TabIndex = 2;
             this.label1.Text = "10:30";
             // 
@@ -78,13 +57,23 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Noto Sans CJK KR Thin", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(34, 139);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(184, 40);
+            this.label2.Size = new System.Drawing.Size(188, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "2017-03-02, 수";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1103, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 85);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -93,10 +82,9 @@
             this.BackgroundImage = global::lock_client.Properties.Resources.무제_1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 768);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -112,12 +100,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- lock_client/lock_client/Form1.cs
+++ lock_client/lock_client/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,90 @@ namespace lock_client
 {
     public partial class Form1 : Form
     {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_SYSKEYUP = 0x0105;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static int _hookID = 0;
+        private delegate int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT IParam);
+        private static int HookCallback(int nCode, int wParam, ref KBDLLHOOKSTRUCT IParam)
+        {
+            bool bReturn = false;
+            switch (wParam)
+            {
+                case WM_KEYDOWN:
+                case WM_KEYUP:
+                case WM_SYSKEYDOWN:
+                case WM_SYSKEYUP:
+
+                    bReturn = ((IParam.vkCode == 0x09) && (IParam.flags == 0x20)) ||    //Alt + Tab
+                        ((IParam.vkCode == 0x1B) && (IParam.flags == 0x20)) ||  //Alt + Esc
+                        ((IParam.vkCode == 0x1B) && (IParam.flags == 0x00)) ||  //Ctrl + Esc
+                        ((IParam.vkCode == 0x5B) && (IParam.flags == 0x01)) ||  //Left Windows Key
+                        ((IParam.vkCode == 0x5C) && (IParam.flags == 0x01)) ||  //Right Windows Key
+                        ((IParam.vkCode == 0x73) && (IParam.flags == 0x20));    //Alt + F4
+
+                    break;
+            }
+            if (bReturn == true)
+                return 1;
+            else
+                return CallNextHookEx(0, nCode, wParam, ref IParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SetWindowsHookEx(int idHook, LowLevelKeyboardProc Ipfn, IntPtr hMod, uint dwThredId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(int hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int CallNextHookEx(int hhk, int nCode, int wParam, ref KBDLLHOOKSTRUCT IParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string IpModulName);
+
+        public struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+        //
+
+        private static int SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+
+            }
+        }
+
+        //Ctrl + Alt + Delete 막기
+        public static void KillCtrlAltDelete()
+        {
+            RegistryKey regkey;
+            string keyValueInt = "1";
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                regkey.SetValue("DisableTaskMgr", keyValueInt);
+                regkey.Close();
+            }
+            catch (Exception ex)
+            {
+                //  MessageBox.Show(ex.ToString());
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +116,8 @@ namespace lock_client
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            KillCtrlAltDelete();
+            _hookID = SetHook(_proc);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -43,18 +131,26 @@ namespace lock_client
         private bool _altF4Pressed;
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Alt && e.KeyCode == Keys.F4)
-                _altF4Pressed = true;
+            if (e.KeyCode == Keys.F4)
+            {
+                e.Handled = true;
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (_altF4Pressed)
-            {
-                if (e.CloseReason == CloseReason.UserClosing)
+            //if (_altF4Pressed)
+            //{
+            //    if (e.CloseReason == CloseReason.UserClosing)
                     e.Cancel = true;
                 _altF4Pressed = false;
-            }
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+            Application.Exit();
         }
     }
 }
